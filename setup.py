6fbb0d7d668d63b2bc4d8f1d3a85257fee1ed184
Giriraj <graj05645@gmail.com>
2024-01-28 18:41:27 +0530

    create logging and exception file

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements (file_path:str) -> List[str]:
+    '''
+    Function Description
+    INPUT: A string of filepath of requirements/requires text file.
+    RETURN: A list of requirments.
+    '''
+    
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [require.replace("\n", "") for require in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+
+setup(
+    name='Student Performance Indicator',
+    version='0.0.1',
+    author='Giriraj',
+    author_email='graj05645@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
